b38b4a8c4184330fb9d01cdc5ace63603545c37c
damianoduranti <42733349+damianoduranti@users.noreply.github.com>
2022-06-29 17:35:22 +0200

    Dashboard
    - Dashboard fixed
    - Requirements and Dockerfile(s) updated

--- src/dashboard/dashboard.py
+++ src/dashboard/dashboard.py
@@ -1,18 +1,69 @@
-from turtle import bgcolor
-from unicodedata import numeric
-from src import crypto_classes
 import streamlit as st # web development
 import numpy as np # np mean, np random 
 import pandas as pd # read csv, df manipulation
 import time # to simulate a real time data, time loop 
 import plotly.express as px # interactive charts 
 import psycopg2
-from tqdm import tqdm
 from datetime import datetime , timedelta, time
 import time 
 import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 
+#Auxiliary functions -----------------------------------------------------------------------------------------------------------
+class Coin_at_time:
+
+    def __init__(self, date, time, open :float  , high:float, low:float, close:float, volume:float)->None:
+        self._date = date
+        self._time = time
+        self._open = open
+        self._high = high
+        self._low = low
+        self._close = close
+        self._volume = volume
+
+    def __repr__(self) -> str:
+        to_ret = { "date " : self._date,
+                "time " : self._time,
+                "open" : self._open,
+                "high" : self._high, 
+                "low" : self._low,
+                "close" : self._close,
+                "volume" : self._volume,}
+        return str(to_ret)
+    
+    def feature_vector(self)-> list:
+        return [self._open , self._high , self._low , self._close , self._volume]
+
+
+class Coin_creator:
+    
+    def Coin_from_binance(raw_data:list):
+        return Coin_at_time(raw_data[0],raw_data[1],raw_data[2],raw_data[3],raw_data[4],raw_data[5],raw_data[6],raw_data[7])
+
+    def Coin_from_CSV(raw_data:list):
+        return Coin_at_time(raw_data[0],raw_data[1],float(raw_data[2]),float(raw_data[3]),float(raw_data[4]),float(raw_data[5]), float(raw_data[6]))
+    
+    def Coin_from_DB(raw_data:list):
+        return Coin_at_time(raw_data[0],raw_data[1],float(raw_data[2]),float(raw_data[3]),float(raw_data[4]),float(raw_data[5]), float(raw_data[6]))
+
+
+"""List with max len"""
+
+class list_max_len:
+
+    def __init__(self, max_len) -> None:
+        self._list = []
+        self._max_len = max_len
+
+    def access_list(self):
+        return self._list
+
+    def add(self, newel):
+        if len(self._list) < self._max_len:
+            self._list.append(newel)
+        else:
+            self._list.pop(0)
+            self._list.append(newel)
 
 # Credentials for the DB ----------------------------------------------------------------------------------------------
 
@@ -69,11 +120,11 @@ df = pd.DataFrame(df)
 
 cur.execute("select * from %s ORDER BY id DESC LIMIT 10" %chosen_coin)
 last10 = cur.fetchall()
-last10_coins = [crypto_classes.Coin_creator.Coin_from_DB(el) for el in last10 ]
+last10_coins = [Coin_creator.Coin_from_DB(el) for el in last10 ]
 last10_preds = [ extract_prediction(el.feature_vector(), chosen_coin) for el in last10_coins]
 last10_preds.reverse()
 
-last10_price = [crypto_classes.Coin_creator.Coin_from_DB(el) for el in last10 ]
+last10_price = [Coin_creator.Coin_from_DB(el) for el in last10 ]
 last10_price = [ el._open for el in last10_price]
 last10_price.reverse()
 
@@ -94,7 +145,7 @@ for i in range(2000):
     # Transformations
     cur.execute("select * from %s ORDER BY id DESC LIMIT 1" %chosen_coin)
     one = cur.fetchone()    
-    my_coin = crypto_classes.Coin_creator.Coin_from_DB(one)    
+    my_coin = Coin_creator.Coin_from_DB(one)    
 
     # Prediction
     feature_vector = my_coin.feature_vector()
